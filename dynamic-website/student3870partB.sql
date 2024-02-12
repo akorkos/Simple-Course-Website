1b3f11cddc10a56110d0a6e1de1a1536962d0818
Alexandros Korkos <alexkork@csd.auth.gr>
2024-02-12 14:48:35 +0200

    Database changes;

--- dynamic-website/student3870partB.sql
+++ dynamic-website/student3870partB.sql
@@ -1,140 +1,172 @@
--- MySQL dump 10.16  Distrib 10.1.48-MariaDB, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
--- Host: webpagesdb.it.auth.gr    Database: student3870partB
--- ------------------------------------------------------
--- Server version	10.1.48-MariaDB-0ubuntu0.18.04.1
+-- Host: webpagesdb.it.auth.gr:3306
+-- Generation Time: Feb 12, 2024 at 02:45 PM
+-- Server version: 10.1.48-MariaDB-0ubuntu0.18.04.1
+-- PHP Version: 7.1.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Current Database: `student3870partB`
+-- Database: `student3870partB`
 --
 
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `announcements`
 --
 
-DROP TABLE IF EXISTS `announcements`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `announcements` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `date` date NOT NULL,
   `subject` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `text` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `text` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `announcements`
 --
 
-LOCK TABLES `announcements` WRITE;
-/*!40000 ALTER TABLE `announcements` DISABLE KEYS */;
-INSERT INTO `announcements` VALUES (1,'2023-10-01','Έναρξη μαθημάτων.','Τα μαθήματα αρχίζουν την Δευτέρα 05/10/2023.'),(2,'2023-12-26','Υποβλήθηκε η εργασία 2','Η 2η εργασία έχει ανακοινωθεί στην ιστοσελίδα<a href=\'./homework.php\'>«Εργασίες»</a>.');
-/*!40000 ALTER TABLE `announcements` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `announcements` (`id`, `date`, `subject`, `text`) VALUES
+(1, '2023-10-01', 'Έναρξη μαθημάτων.', 'Τα μαθήματα αρχίζουν την Δευτέρα 05/10/2023.'),
+(2, '2023-12-26', 'Υποβλήθηκε η εργασία 2', 'Η 2η εργασία έχει ανακοινωθεί στην ιστοσελίδα<a href=\'./homework.php\'> Εργασίες</a>.');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `documents`
 --
 
-DROP TABLE IF EXISTS `documents`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `documents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `title` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `file_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `file_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `documents`
 --
 
-LOCK TABLES `documents` WRITE;
-/*!40000 ALTER TABLE `documents` DISABLE KEYS */;
-INSERT INTO `documents` VALUES (1,'Απλά προβλήματα υπολογισμού και χρήσης ποσοστών','Απλά προβλήματα υπολογισμού και χρήσης ποσοστών.','../files/file1.doc'),(2,'Παραδείγματα συναρτήσεων και εισαγωγή στην εμβέλεια των μεταβλητών.','Παραδείγματα συναρτήσεων και εισαγωγή στην εμβέλεια των μεταβλητών.','../files/file2.doc');
-/*!40000 ALTER TABLE `documents` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `documents` (`id`, `title`, `description`, `file_name`) VALUES
+(2, 'Παραδείγματα συναρτήσεων και εισαγωγή στην εμβέλεια των μεταβλητών.', 'Παραδείγματα συναρτήσεων και εισαγωγή στην εμβέλεια των μεταβλητών.', '../files/file2.doc');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `homeworks`
 --
 
-DROP TABLE IF EXISTS `homeworks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `homeworks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `targets` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `file_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `files_needed` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `deadline` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `deadline` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `homeworks`
 --
 
-LOCK TABLES `homeworks` WRITE;
-/*!40000 ALTER TABLE `homeworks` DISABLE KEYS */;
-INSERT INTO `homeworks` VALUES (1,'1.Υλοποίηση ενός AVL Tree.\r\n2.Εισαγωγή στην γλώσσα προγραμματισμού C++.\r\n','../files/ergasia1.doc','1.Γραπτή αναφορά σε PDF.\r\n2.Αρχειο με τον πηγαίο κώδικα.','2024-02-23'),(2,'1.Υλοποίηση μιας δομής R tree.\r\n2.Εξηκειωση με την γλώσσα προγραμματισμού Java.\r\n3.Συνεργατική δουλειά.','../files/ergasia2.doc','1.Γραπτή αναφορά σε word.\r\n2.Αρχειο με τον πηγαίο κώδικα.','2023-02-15');
-/*!40000 ALTER TABLE `homeworks` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `homeworks` (`id`, `targets`, `file_name`, `files_needed`, `deadline`) VALUES
+(1, '1.Υλοποίηση ενός Sorting αλγόριθμου.\r\n2.Εισαγωγή στην γλώσσα προγραμματισμού C++.\r\n', '../files/ergasia1.doc', '1.Γραπτή αναφορά σε PDF.\r\n2.Αρχειο με τον πηγαίο κώδικα.', '2024-02-23'),
+(2, '1.Υλοποίηση μιας δομής δεδομένων.\r\n2.Εξοικείωση με την γλώσσα προγραμματισμού Java.\r\n3.Ομαδική δουλειά.', '../files/ergasia2.doc', '1.Γραπτή αναφορά σε word.\r\n2.Αρχειο με τον πηγαίο κώδικα', '2023-02-15');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `surname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` enum('tutor','student') COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `role` enum('tutor','student') COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Alexandros','Korkos','kork@auth.gr','123','tutor'),(2,'Alkis','Tsagkaris','tsagkar@auth.gr','123','student'),(3,'Evgenios','Tzounidis','tzoun@auth.gr','123','student'),(4,'Panos','Stergiopoulos','stergio@auth.gr','123','tutor');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `role`) VALUES
+(1, 'Alexandros', 'Korkos', 'kork@auth.gr', '123', 'tutor'),
+(2, 'Alkis', 'Tsagkaris', 'tsagkar@auth.gr', '123', 'student'),
+(3, 'Evgenios', 'Tzounidis', 'tzoun@auth.gr', '123', 'student'),
+(4, 'Panos', 'Stergiopoulos', 'stergio@auth.gr', '123', 'tutor');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `homeworks`
+--
+ALTER TABLE `homeworks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `homeworks`
+--
+ALTER TABLE `homeworks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-01-20 22:00:50
-
